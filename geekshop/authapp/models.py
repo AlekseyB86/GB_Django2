61d9f274538f24c9117c8934e85ba0c3f85c112e
AlekseyB86 <avbelov86@mail.ru>
2021-12-29 15:13:41 +0300

    lesson2 - vk_oauth2: create userprofile - add update_db

--- geekshop/authapp/models.py
+++ geekshop/authapp/models.py
@@ -2,7 +2,11 @@ from datetime import timedelta
 
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 from django.utils.timezone import now
+
+NULLABLE = {'null': True, 'blank': True}
 
 
 class User(AbstractUser):
@@ -10,7 +14,29 @@ class User(AbstractUser):
     age = models.PositiveIntegerField(default=18)
 
     activation_key = models.CharField(max_length=128, blank=True)
-    activation_key_expires = models.DateTimeField(auto_now=True, blank=True, null=True)
+    activation_key_expires = models.DateTimeField(auto_now=True, **NULLABLE)
 
     def is_activation_key_expires(self):
         return now() > self.activation_key_expires + timedelta(hours=48)
+
+
+class UserProfile(models.Model):
+    MALE = 'M'
+    FEMALE = 'W'
+    GENDER_CHOICES = (
+        (MALE, 'М'),
+        (FEMALE, 'Ж'),
+    )
+
+    user = models.OneToOneField(User, unique=True, null=False, db_index=True, on_delete=models.CASCADE)
+    about = models.TextField(verbose_name='о себе', **NULLABLE)
+    gender = models.CharField(verbose_name='пол', choices=GENDER_CHOICES, blank=True, max_length=1)
+
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            UserProfile.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, created, **kwargs):
+        instance.userprofile.save()
